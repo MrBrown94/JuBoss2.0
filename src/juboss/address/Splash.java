a2aa407484274018911812a5440cf1db1a900fa5
Fabio Di Carlo <fhumoo@gmail.com>
2017-04-03 16:42:12 +0200

    Aggiunto ColoreVino sul DB e Classe Splash

---
+++ src/juboss/address/Splash.java
@@ -0,0 +1,62 @@
+package juboss.address;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Splash extends JWindow {
+	
+	private static final long serialVersionUID = 1L;
+	private int duration;
+	
+	
+	//Costruttore
+	public Splash(int d) {
+		
+		duration = d;
+	}
+
+	
+	//View Splash JPanel
+	public void showSplash() {
+		
+		//Creazione JPanel
+		JPanel content = (JPanel)getContentPane();
+		content.setBackground(new Color(0, 0, 0, 255));
+
+		//Center JPanel
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (screen.width-500)/2;
+		int y = (screen.height-648)/2;
+		setBounds(x,y,500,648);
+
+  
+	    //Apertura GIF
+		ImageIcon bg = new ImageIcon(Splash.class.getResource("/splashsmall.gif"));
+		JLabel label = new JLabel(bg);
+		
+		content.add(label, BorderLayout.CENTER);
+
+		//Display Splash
+		setVisible(true);
+
+		//Sleep Splash
+	    try { Thread.sleep(duration); } catch (Exception e) {}
+	    dispose();
+	}
+	
+	
+	//Start MainApp a timer scaduto
+	public void showSplashAndStart(){
+			
+		showSplash();
+		MainApp.main(null);
+	}
+
+	
+	//Main Splash
+	public static void main(String[] args) {
+		
+		Splash splash = new Splash(3000);
+	    splash.showSplashAndStart();
+	  }
+}
