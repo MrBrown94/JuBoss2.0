0631ffcf95ef9356c30497d08f23a3acb0fb030b
Simone <simonbrown994@gmail.com>
2017-04-07 19:43:37 +0200

    addedit behaviour

--- src/juboss/view/AddEditController.java
+++ src/juboss/view/AddEditController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -12,6 +13,8 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import juboss.model.Wine;
 
 public class AddEditController {
 
@@ -50,9 +53,14 @@ public class AddEditController {
 
     @FXML
     private TextField fieldIngrosso;
+    
+    @FXML
+    private AnchorPane root;
 
     @FXML
     private TextField fieldDettaglio;
+    
+    int[] settings;
 
     @FXML
     void initialize() {
@@ -67,29 +75,46 @@ public class AddEditController {
         assert textAreaNote != null : "fx:id=\"textAreaNote\" was not injected: check your FXML file 'AddEdit.fxml'.";
         assert fieldIngrosso != null : "fx:id=\"fieldIngrosso\" was not injected: check your FXML file 'AddEdit.fxml'.";
         assert fieldDettaglio != null : "fx:id=\"fieldDettaglio\" was not injected: check your FXML file 'AddEdit.fxml'.";
+        assert root != null : "fx:id=\"root\" was not injected: check your FXML file 'AddEdit.fxml'.";
+       
         
+        settings = juboss.Splash.db.getSettings();
+        
+        //check on fieldPrezzo content
         
         fieldPrezzo.focusedProperty().addListener(new ChangeListener<Boolean>()
         {
             @Override
             public void changed(ObservableValue<? extends Boolean> arg0, Boolean oldPropertyValue, Boolean newPropertyValue)
             {
-                if (newPropertyValue)
+                if (!newPropertyValue)
+                   
                 {
-                    System.out.println("Textfield on focus");
-                }
-                else
-                {
-                    System.out.println("Textfield out focus");
                     fieldPrezzo.setText(fieldPrezzo.getText().replaceAll(",", "."));
-                    if(!fieldPrezzo.getText().contains("."))
-                    	fieldPrezzo.setText(fieldPrezzo.getText().concat(".00"));
+                    
+	                
+	                    if(!fieldPrezzo.getText().contains(".") && !fieldPrezzo.getText().equals("") )
+		                    	fieldPrezzo.setText(fieldPrezzo.getText().concat(".00"));
                     
                 }
             }
         });
+        
+        
+		fieldPrezzo.textProperty().addListener(new ChangeListener<String>() {
+		    @Override
+		    public void changed(ObservableValue<? extends String> observable,
+		            String oldValue, String newValue) {
+		    	
+		    //don't calculate when manual mode is enabled	
+		    	if(!checkManuale.isSelected())
+					percentCalc();
 
-    }
+		    }
+		});
+     }
+    
+    //check denominazione and insert
     @FXML
     public void checkAndInsert(){
     	
@@ -109,7 +134,10 @@ public class AddEditController {
     	if(fieldDenominazione.getText().equals(""))
     		alert.showAndWait();
     	
-    	else 
+    	
+    	else
+    		//if prezzo dettaglio or ingrosso are empty , replace content with "0"
+    		
     		if(fieldPrezzo.getText().equals("")||fieldIngrosso.getText().equals("")||fieldDettaglio.getText().equals(""))
 	    		{
 	    			juboss.Splash.db.insertData(	fieldDenominazione.getText().toUpperCase(), 
@@ -129,8 +157,15 @@ public class AddEditController {
     				juboss.Splash.viniOb = juboss.Splash.db.getAllData();
     				
     				aggiunto.showAndWait();
+    				
+    				//clean form    				
+    				
+    				cleanForm();
+	    		
 	    		}
 		    	
+    		//if prezzo dettaglio or ingrosso are NOT empty , puts content on db
+
 	    		else{
 	    				juboss.Splash.db.insertData(	fieldDenominazione.getText().toUpperCase(), 
 	    												fieldProduttore.getText().toUpperCase(), 
@@ -145,19 +180,76 @@ public class AddEditController {
 		    											checkManuale.isSelected()
 		    										);
     				
-    			    //update lista dopo inserimento
+    			    //update list after insert
     				juboss.Splash.viniOb = juboss.Splash.db.getAllData();
     				
     				aggiunto.showAndWait();
     				
-    		    	
+    				//clean form
+    				
+    				cleanForm();
     				
     			};
-    		
+    			
     }
     
+    //grab focus on click on the bg
+	@FXML
+	void grabFocus(){
+		
+		root.requestFocus();
+	}
+    
+	//fill dettaglio and ingrosso with values calculated by options
+	@FXML
+	void percentCalc(){
 	
-    @FXML
-    void manualMode(){}
+		if(!fieldPrezzo.getText().equals(""))
+		{
+			fieldDettaglio.setText(Double.toString( Double.parseDouble(fieldPrezzo.getText()) + Double.parseDouble(fieldPrezzo.getText())/100*settings[0]) );
+			fieldIngrosso.setText(Double.toString( Double.parseDouble(fieldPrezzo.getText()) + Double.parseDouble(fieldPrezzo.getText())/100*settings[1]) );
+		}
+		else 
+		{
+			fieldDettaglio.setText("");
+			fieldIngrosso.setText("");
+		}
+	}
+	
+    //unlock dettaglio and ingrosso
+	@FXML
+    void manualMode(){
+    	
+    	if(checkManuale.isSelected())
+	    	{
+	    	fieldDettaglio.setDisable(false);
+	    	fieldIngrosso.setDisable(false);
+	    	fieldDettaglio.clear();
+	    	fieldIngrosso.clear();
+	    	}
+    			else
+    			{
+    				fieldDettaglio.setDisable(true);
+    				fieldIngrosso.setDisable(true);
+    				percentCalc();
+    			}
+    	
+    }
+    
+   //cleans the form
+    void cleanForm()
+    {
+    	fieldDenominazione.clear();
+		fieldProduttore.clear(); 
+		fieldTipoVino.clear();
+		comboPaese.getSelectionModel().select(null); 
+		comboRegione.getSelectionModel().select(null);
+		fieldCapacità.clear();
+		textAreaNote.clear(); 
+		fieldPrezzo.clear(); 
+		fieldIngrosso.clear(); 
+		fieldDettaglio.getText();
+		checkManuale.setSelected(false);
+    }
     
 }
